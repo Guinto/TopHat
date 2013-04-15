b7131b14713d9f3291a4acb5c59ad3630f556f19
Trent Ellingsen <trent.ellingsen@mindbodyonline.com>
2013-04-15 13:19:05 -0700

    initial project commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TopHat::Application.config.secret_token = '8d1102754aa06dc04ce2ed0091a6a6a456a8b6152f62d7291fb14ae1f222f802a6fa43e1e3941683a61fbfd9b4be9b0bcc2f5e46b75848db4b2359af0de860c7'
